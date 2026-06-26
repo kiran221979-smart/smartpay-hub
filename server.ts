6292e44c745100ba4a5ea092840c647dfb4c9e57
kiran221979-smart <kiran221979@gmail.com>
2026-06-27 01:35:47 +0530

    Add files via upload

--- server.ts
+++ server.ts
@@ -1,7 +1,5 @@
-import "./lib/error-capture";
-
-import { consumeLastCapturedError } from "./lib/error-capture";
-import { renderErrorPage } from "./lib/error-page";
+import { consumeLastCapturedError } from "@/lib/error-capture";
+import { renderErrorPage } from "@/lib/error-page";
 
 type ServerEntry = {
   fetch: (request: Request, env: unknown, ctx: unknown) => Promise<Response> | Response;
